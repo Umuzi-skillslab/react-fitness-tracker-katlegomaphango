e912f7f33749c916a25ea60e89360efb26851d2f
KatIvan <mphangokatlego08@gmail.com>
2026-08-31 09:04:35 +0000

    nav

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link, useLocation } from 'react-router-dom'
 
 function Navbar() {
     return (
@@ -5,13 +6,11 @@ function Navbar() {
             <div>
                 <h1>Fit Tipper</h1>
                 <nav>
-                    <ul>
-                        <li>Dashboard</li>
-                        <li>Exercises</li>
-                        <li>Workout Planner</li>
-                        <li>Workout Log</li>
-                        <li>Progress</li>
-                    </ul>
+                    <Link to="/">Home</Link>
+                    <Link to="/exercises">Exercises</Link>
+                    <Link to="/workout-planner">Workout Planner</Link>
+                    <Link to="/history">History</Link>
+                    <Link to="/progress">Progress</Link>
                 </nav>
             </div>
         </>
@@ -19,3 +18,70 @@ function Navbar() {
 }
 
 export default Navbar
+
+{/**
+    <nav className={styles.navbar}>
+
+      <Link 
+
+        to="/" 
+
+        className={location.pathname === '/' ? styles.active : ''}
+
+      >
+
+        Home
+
+      </Link>
+
+      <Link 
+
+        to="/exercises"
+
+        className={location.pathname.includes('/exercises') ? styles.active : ''}
+
+      >
+
+        Exercises
+
+      </Link>
+
+      <Link 
+
+        to="/workout-planner"
+
+        className={location.pathname === '/workout-planner' ? styles.active : ''}
+
+      >
+
+        Workout Planner
+
+      </Link>
+
+      <Link 
+
+        to="/history"
+
+        className={location.pathname === '/history' ? styles.active : ''}
+
+      >
+
+        History
+
+      </Link>
+
+      <Link 
+
+        to="/progress"
+
+        className={location.pathname === '/progress' ? styles.active : ''}
+
+      >
+
+        Progress
+
+      </Link>
+
+    </nav>
+
+    */}
