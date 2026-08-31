50546f8d61f5f586e42f35978567129dd0a50cb5
KatIvan <mphangokatlego08@gmail.com>
2026-08-31 11:09:33 +0000

    nav and mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Navbar from './components/Navigation/Navbar'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -7,7 +9,21 @@ function App() {
     <>
       <div>
         <Navbar />
+
         <h1>Hello world</h1>
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/exercises" element={<ExercisesPage />} />
+          <Route path="/exercises/:id" element={<ExerciseDetail />} />
+          <Route path="/workout-planner" element={<WorkoutPlannerPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          <Route path="*" element={<NotFound />} /> */}
+
+        </Routes>
+
       </div>
     </>
   )
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,18 +1,90 @@
 import { Link, useLocation } from 'react-router-dom'
+import { AppBar, Box, Container, IconButton, Toolbar, Typography, Menu, MenuItem } from '@mui/material'
+import { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+
 
 function Navbar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
     return (
         <>
-            <div>
-                <h1>Fit Tipper</h1>
-                <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/exercises">Exercises</Link>
-                    <Link to="/workout-planner">Workout Planner</Link>
-                    <Link to="/history">History</Link>
-                    <Link to="/progress">Progress</Link>
-                </nav>
-            </div>
+            <AppBar position='fixed'>
+              <Container maxWidth="xl">
+                {/* <AdbIcon /> */}
+                <Toolbar>
+                  <Typography
+                    variant='h6'
+                    noWrap
+                    component="a"
+                    sx={{
+                      mr: 2,
+                      display: { xs: 'none', md: 'flex' },
+                      fontWeight: 700,
+                      textDecoration: 'none'
+                    }}
+                  >Fit Tipper</Typography>
+
+                  <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none'}}}>
+                    <IconButton
+                      size='large'
+                      onClick={handleOpenNavMenu}
+                      color='inherit'
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorElNav}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left'
+                      }}
+                      open={Boolean(anchorElNav)}
+                      onClose={handleCloseNavMenu}
+                      sx={{ display: { xs: 'block', md: 'none' }}}
+                    >
+                      <MenuItem key={'home'} onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center'}}>Home</Typography>
+                      </MenuItem>
+                      <MenuItem key={'exercises'} onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center'}}>Exercises</Typography>
+                      </MenuItem>
+                      <MenuItem key={'workout-planner'} onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center'}}>Workout Planner</Typography>
+                      </MenuItem>
+                      <MenuItem key={'history'} onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center'}}>History</Typography>
+                      </MenuItem>
+                      <MenuItem key={'progress'} onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center'}}>Progress</Typography>
+                      </MenuItem>
+                    </Menu>
+                  </Box>
+                  <nav>
+                      <Link to="/">Home</Link>
+                      <Link to="/exercises">Exercises</Link>
+                      <Link to="/workout-planner">Workout Planner</Link>
+                      <Link to="/history">History</Link>
+                      <Link to="/progress">Progress</Link>
+                  </nav>
+                </Toolbar>
+              </Container>
+            </AppBar>
         </>
     )
 }
