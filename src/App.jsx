7d5b64454ee48c08b87f02d2fe49437f04c08de0
KatIvan <mphangokatlego08@gmail.com>
2026-08-31 08:35:48 +0000

    init nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navigation/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
         <h1>Hello world</h1>
       </div>
     </>
---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,21 @@
+
+function Navbar() {
+    return (
+        <>
+            <div>
+                <h1>Fit Tipper</h1>
+                <nav>
+                    <ul>
+                        <li>Dashboard</li>
+                        <li>Exercises</li>
+                        <li>Workout Planner</li>
+                        <li>Workout Log</li>
+                        <li>Progress</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
