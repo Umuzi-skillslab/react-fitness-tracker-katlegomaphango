55344701e62d0c0638de2261961450f6b1c34504
katlego <mphangokatlego08@gmail.com>
2026-09-03 14:59:32 +0200

    refactor

--- src/components/Exercise/ExerciseList.jsx
+++ src/components/Exercise/ExerciseList.jsx
@@ -3,12 +3,15 @@ import { Grid } from '@mui/material';
 
 function ExerciseList({ exercises, workoutPlan = [], onAddToWorkout, onSelectExercise }) {
   return (
-    <Grid container spacing={3} sx={{ display: 'flex', justifyContent: 'center' }} xs={12} sm={6} md={4}>
+    <Grid container spacing={3} sx={{ width: "100%" }}>
       {exercises.map((exercise) => (
         <Grid
           item
           key={exercise.id}
-          sx={{ display: 'flex', justifyContent: 'center' }}
+          sx={{ display: 'flex', }}
+          xs={12}
+          sm={6} 
+          md={4}
         >
           <ExerciseCard
             exercise={exercise}
